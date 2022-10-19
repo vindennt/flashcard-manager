0e0fe8196bb82329423db6d1294251b45468c1ad
dennistruong <dent00@student.ubc.ca>
2022-10-18 21:32:20 -0700

    Added framework for ui

--- src/main/ui/FlashCardApp.java
+++ src/main/ui/FlashCardApp.java
@@ -1,6 +1,14 @@
 package ui;
 
+import model.Deck;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class FlashCardApp {
+    private Scanner input;
+    private ArrayList<Deck> deckList;
+
     public FlashCardApp() {
         runFlashCardApp();
     }
@@ -8,19 +16,58 @@ public class FlashCardApp {
     private void runFlashCardApp() {
         boolean proceed = true;
         String command = null;
-
         init();
 
         while (proceed) {
-            displayMenu();
+            displayMainMenu();
             command = input.next();
             command = command.toLowerCase();
 
-            if (command.equals("q")) {
+            if (command.equals("x")) {
                 proceed = false;
             } else {
                 processCommand(command);
             }
         }
+        System.out.println("Exiting...");
     }
+
+    private void init() {
+        deckList = new ArrayList<>();
+        input = new Scanner(System.in);
+        input.useDelimiter("\n");
+    }
+
+    private void displayMainMenu() {
+        System.out.println("\nSelect command:");
+        System.out.println("\tn -> new deck");
+        System.out.println("\te -> edit deck");
+        System.out.println("\tr -> review deck");
+        System.out.println("\tx -> exit");
+    }
+
+    private void processCommand(String command) {
+        if (command.equals("a")) {
+            doAddNewDeck();
+        } else if (command.equals("e")) {
+            doEditDeck();
+        } else if (command.equals("r")) {
+            doReviewDeck();
+        } else {
+            System.out.println("Invalid command");
+        }
+    }
+
+    private void doReviewDeck() {
+    }
+
+    private void doEditDeck() {
+        
+    }
+
+    private void doAddNewDeck() {
+        
+    }
+
+
 }
