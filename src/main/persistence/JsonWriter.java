53525d81eda387060a68de83631d0b8888980c38
dennistruong <truong.dennis00@gmail.com>
2022-10-25 12:09:29 -0700

    Reworked JsonWriter to fit classes from this project
    To do: rework JsonWriter. write tests for all with test files, update ui, run practical tests

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -2,36 +2,46 @@
 
 package persistence;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import model.Deck;
 import org.json.JSONObject;
 
+
+import java.io.*;
+
+// Represents a writer that writes JSON representation of workroom to file
 public class JsonWriter {
-    public static final int TAB = 4;
+    private static final int TAB = 4;
     private PrintWriter writer;
     private String destination;
 
+    // EFFECTS: constructs writer to write to destination file
     public JsonWriter(String destination) {
         this.destination = destination;
     }
 
+    // MODIFIES: this
+    // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
+    // be opened for writing
     public void open() throws FileNotFoundException {
-        this.writer = new PrintWriter(new File(this.destination));
+        writer = new PrintWriter(new File(destination));
     }
 
+    // MODIFIES: this
+    // EFFECTS: writes JSON representation of deck to file
     public void write(Deck d) {
-        // TODO implement into deck
         JSONObject json = d.toJson();
-        this.saveToFile(json.toString(TAB));
+        saveToFile(json.toString(TAB));
     }
 
+    // MODIFIES: this
+    // EFFECTS: closes writer
     public void close() {
-        this.writer.close();
+        writer.close();
     }
 
+    // MODIFIES: this
+    // EFFECTS: writes string to file
     private void saveToFile(String json) {
-        this.writer.print(json);
+        writer.print(json);
     }
 }
